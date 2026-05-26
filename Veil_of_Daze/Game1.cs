ab2e8a23452ef43243b55b4b907ba981c07fb693
Moonismylover <Angelpreetthind9@gmail.com>
2026-05-26 14:38:49 -0400

    4567890

--- Veil_of_Daze/Game1.cs
+++ Veil_of_Daze/Game1.cs
@@ -29,12 +29,19 @@ namespace Veil_of_Daze
         Texture2D butterfly;
         Rectangle butterflyRect;
 
-        Texture2D flower;
-        Rectangle flowerRect;
+        Texture2D play;
+        Rectangle playRect;
+
+        Texture2D menu;
+        Rectangle menuRect;
+
+        Texture2D exit;
+        Rectangle exitRect;
 
         float seconds;
 
         MouseState mouseState;
+        MouseState previousMouseState;
 
         public Game1()
         {
@@ -53,9 +60,11 @@ namespace Veil_of_Daze
             _graphics.ApplyChanges();
 
             mainTextRect = new Rectangle(30, 130, 500, 300);
-            flowerRect = new Rectangle(370, 190, 100, 100);
             butterflyRect = new Rectangle(60, 250, 150, 150);
-
+            playRect = new Rectangle(370, 160, 120, 50);
+            menuRect = new Rectangle(370, 210, 120, 50);
+            exitRect = new Rectangle(370, 260, 120, 50);
+ 
             seconds = 0;
 
             base.Initialize();
@@ -68,7 +77,9 @@ namespace Veil_of_Daze
             mainBg = Content.Load<Texture2D>("red_bg");
             mainText = Content.Load<Texture2D>("main_text");
             butterfly = Content.Load<Texture2D>("butterfly");
-            flower = Content.Load<Texture2D>("flower");
+            play = Content.Load<Texture2D>("play");
+            menu = Content.Load<Texture2D>("menu");
+            exit = Content.Load<Texture2D>("exit");
         }
 
         protected override void Update(GameTime gameTime)
@@ -76,6 +87,7 @@ namespace Veil_of_Daze
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            previousMouseState = mouseState;
             mouseState = Mouse.GetState();
 
             this.Window.Title = mouseState.Position.ToString();
@@ -84,10 +96,25 @@ namespace Veil_of_Daze
 
             if (screen == Screen.main)
             {
-                if (mouseState.LeftButton == ButtonState.Pressed)
+                if (mouseState.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton == ButtonState.Released && playRect.Contains(mouseState.Position))
+                {
+                    screen = Screen.veilOfDaze;
+                }
+
+                if (mouseState.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton == ButtonState.Released && menuRect.Contains(mouseState.Position))
                 {
                     screen = Screen.menu;
                 }
+
+                if (mouseState.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton == ButtonState.Released && exitRect.Contains(mouseState.Position))
+                {
+                    Exit();
+                }
+            }
+            
+            if (screen == Screen.menu)
+            {
+
             }
 
 
@@ -105,7 +132,9 @@ namespace Veil_of_Daze
                 _spriteBatch.Draw(mainBg, window, Color.White);
                 _spriteBatch.Draw(mainText, mainTextRect, Color.White);
                 _spriteBatch.Draw(butterfly, butterflyRect, Color.White);
-                _spriteBatch.Draw(flower, flowerRect, Color.White);
+                _spriteBatch.Draw(play, playRect, Color.White);
+                _spriteBatch.Draw(menu, menuRect, Color.White);
+                _spriteBatch.Draw(exit, exitRect, Color.White);
             }
             else if (screen == Screen.menu)
             {
